247899c6052cdf9e00cdc199e6af864c0927f8f3
Ashish Bhargava <shishbharg@gmail.com>
2017-10-04 12:15:09 +0530

    Adding empty Java files

---
+++ src/main/java/edu/ashish/SmallestElementinAray.java
@@ -0,0 +1,11 @@
+package edu.ashish;
+
+/**
+ * Created by abha51 on 10/4/2017.
+ */
+public class SmallestElementinAray {
+
+    public void static main(String... args) {
+
+    }
+}
---
+++ src/main/java/edu/ashish/sorts/SelectionSort.java
@@ -0,0 +1,7 @@
+package edu.ashish.sorts;
+
+/**
+ * Created by abha51 on 10/4/2017.
+ */
+public class SelectionSort {
+}
