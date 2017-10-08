a0176564830150d1ddb93c5bdfccb35ca63a82b1
Ashish Bhargava <ashishbharg@gmail.com>
2017-10-08 19:31:57 +0530

    Selection Sort

--- src/main/java/edu/ashish/sorts/SelectionSort.java
+++ src/main/java/edu/ashish/sorts/SelectionSort.java
@@ -4,4 +4,29 @@ package edu.ashish.sorts;
  * Created by abha51 on 10/4/2017.
  */
 public class SelectionSort {
+
+    public static void main(String[] args) {
+        int[] arr = new int[] {5,7,12,2,3,9,10,4};
+        selectionSort(arr);
+        for(int a : arr) {
+            System.out.print(a + " ");
+        }
+    }
+
+    public static void selectionSort(int[] n) {
+        int temp;
+        for(int i=0; i<n.length; i++) {
+            int min = i;
+            for(int j=i+1; j<n.length; j++) {
+                if(n[j] < n[min]) {
+                    min = j;
+                }
+            }
+            if(n[i] > n[min]) {
+                temp = n[i];
+                n[i] = n[min];
+                n[min] = temp;
+            }
+        }
+    }
 }
---
+++ src/main/java/edu/ashish/strings/AsciiToInteger.java
@@ -0,0 +1,24 @@
+package edu.ashish.strings;
+
+/**
+ * Created by ashish.bhargava on 10/6/2017.
+ */
+public class AsciiToInteger {
+
+    public static void main(String[] args) {
+
+        "Hello"
+        atoi("");
+    }
+
+    private static int atoi(String str) {
+
+        if(null == str || str.trim().isEmpty())
+            return 0;
+
+        return 1;
+
+
+
+    }
+}
